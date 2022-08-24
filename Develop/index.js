92a6faa18edde54d3c288e67f11fd601470b65fe
bilalfarrukh2000 <bilalfarrukh2000@gmail.com>
2022-08-23 21:57:40 -0400

    Add questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,13 +4,94 @@ const fs = require('fs');
 const generateMarkdown= require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'Description',
+        message: 'What motivated you to conduct this project?',
+      },
+      {
+        type: 'list',
+        name: 'License',
+        message: 'What is the license for this app?',
+        choices: ['MIT', 'Apache'],
+      },
+      {
+        type: 'input',
+        name: 'Installation',
+        message: 'What steps were required to fully install the project?',
+      },
+      {
+        type: 'input',
+        name: 'Usage',
+        message: 'Can you provide the instructions and examples with screenshots?',
+      },
+      {
+        type: 'input',
+        name: 'Credits',
+        message: 'Can you reveal who and what you used to help you? Please list out your collaborators as well as any third-party assets you used to conduct this project.',
+      },
+      {
+        type: 'input',
+        name: 'Features',
+        message: 'What features does your project provide?',
+      },
+      {
+        type: 'input',
+        name: 'Contribute',
+        message: 'Include guidelines that explain how other developers can contribute to your project.',
+      },
+      {
+        type: 'input',
+        name: 'Tests',
+        message: 'Please run tests and provide examples to show how the user should run your application',
+      },
+      {
+        type: 'input',
+        name: 'ghUserName',
+        message: 'Enter Github Username.',
+      },
+      {
+        type: 'input',
+        name: 'Email',
+        message: 'Enter Email Address.',
+      },
+    ];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve,reject) => {
+        fs.writeFile(fileName,data,e=> {
+            if(e){
+                reject(e);
+                return;
+            }
+            resolve({
+                ok: true
+            })
+        })
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then (function (response){
+        let data=generateMarkdown(response);
+        writeToFile("sampleReadMe.md",data);
+    
+    })
+}
 
 // Function call to initialize app
 init();
+
+
+module.exports = questions;
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,81 @@
+const fs = require('fs');
+const index=require('../index.js');
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+  if(license==='MIT')
+    badge='![badge](https://www.channelfutures.com/files/2017/04/3_0.png)'
+  else if(license==='Apache')
+    badge='![badge](https://media-exp1.licdn.com/dms/image/C4D12AQG4iOEV4ytsKg/article-cover_image-shrink_720_1280/0/1633953703308?e=2147483647&v=beta&t=zHRGYnZU2Xf30Bo4_ewpm9gpLSUjaa6rK94jdPn_1vQ)'
+
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  
+  if(license==='MIT')
+    link='https://choosealicense.com/licenses/mit';
+  else if(license==='Apache')
+   link='https://www.apache.org/licenses/LICENSE-2.0';
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  return link;
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.Title}
+  ## Description:
+
+  ${data.Description}
+
+  ## Table of Contents:
+  ###  [Installation](#installation)
+  ###  [Usage](#usage)
+  ###  [Credits](#credits)
+  ###  [Features](#features)
+  ###  [Contribution Guidelines](#contribution-guidelines)
+  ###  [Tests](#Tests)
+  ###  [License](#License)
+  ###  [Questions](#Questions)
+
+ 
+
+  ## Installation
+  ${data.Installation}
+
+  ## Usage
+  ${data.Usage}
+
+  ## Credits
+  ${data.Credits}
+
+  ## Features
+  ${data.Features}
+
+  ## Contribution Guidelines
+  ${data.Contribute}
+
+  ## Tests
+  ${data.Tests}
+
+  ## License
+  ### This application uses the ${data.License} license
+  ### ${renderLicenseBadge(data.License)}
+  ### Link: ${renderLicenseLink(data.License)};
+
+
+  ## Questions
+  ### Please reach out for any questions at:
+  ### Github: https://github.com/${data.ghUserName}
+  ### or
+  ### Email: ${data.Email}
 
 `;
 }
